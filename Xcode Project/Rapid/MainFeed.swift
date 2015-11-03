d630003a18bc8c78959200ed958ee9f81a28a0e3
shutupdilpa <newaccount@d-172-26-27-121.bootp.virginia.edu>
2015-11-02 22:15:32 -0500

    Added MainFeed and PostTypes swift files

---
+++ Xcode Project/Rapid/MainFeed.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Rapid
+//
+//  Created by Jonathan Bentley on 10/27/15.
+//  Copyright © 2015 Rapid LLC. All rights reserved.
+//
+
+import UIKit
+
+class MainFeed: UITableView {
+    
+    
+}
+
---
+++ Xcode Project/Rapid/PostTypes.swift
@@ -0,0 +1,18 @@
+//
+//  PostTypes.swift
+//  Rapid
+//
+//  Created by new account on 11/2/15.
+//  Copyright © 2015 Rapid LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImagePost: UITableViewCell {
+    
+}
+
+class TextPost: UITableViewCell {
+    
+}
